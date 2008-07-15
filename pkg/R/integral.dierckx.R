4c5ca2caa9035bce4c6f925ae6cefd08f1b9e096
spencerg (FinTS ) <spencer.graves@prodsyse.com>
2008-07-15 01:25:53 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dierckxspline@32 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/integral.dierckx.R
+++ pkg/R/integral.dierckx.R
@@ -1,16 +1,22 @@
-integral <- function(x, ...) {
-  UseMethod("integral")
-}
+#integral <- function(x, ...) {
+#  UseMethod("integral")
+#}
 
-integral.dierckx <- function(x, from = NULL, to = NULL, ...) {
-  from <- if(is.null(from)) min(x$knots) else max(from, min(x$knots))
-  to <- if(is.null(to)) max(x$knots) else min(to, max(x$knots))
-  n <- as.integer(x$n)
+integral.dierckx <- function(expr, from = NULL, to = NULL, ...) {
+  from <- {
+    if(is.null(from)) min(expr$knots)
+    else max(from, min(expr$knots))
+  }
+  to <- { 
+    if(is.null(to)) max(expr$knots)
+    else min(to, max(expr$knots))
+  }
+  n <- as.integer(expr$n)
   int <- .Fortran("splint",
-                  t = as.single(x$knots[seq(n)]),
+                  t = as.single(expr$knots[seq(n)]),
                   n = as.integer(n),
-                  c = as.single(x$coef[seq(n)]),
-                  k = as.integer(x$k),
+                  c = as.single(expr$coef[seq(n)]),
+                  k = as.integer(expr$k),
                   a = as.single(from),
                   b = as.single(to),
                   wrk = single(n),
