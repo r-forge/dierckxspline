95ea0bafdf4e675ce13b2150fae869a6e904c103
spencerg (FinTS ) <spencer.graves@prodsyse.com>
2013-07-04 11:38:05 +0000

    splineZeros to accept uniroot of different lengths
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dierckxspline@51 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/splineZeros.R
+++ pkg/R/splineZeros.R
@@ -11,13 +11,13 @@ splineZeros <- function(object, maxiter=10){
 ##
   cP <- controlPolygon(object)
   cP. <- predict(object, cP[, 1])
-  obj. <- object
 ##
-## 3.  while there are differences in sign, insert knots and iterate 
-##  
+## 3.  while there are differences in sign, insert knots and iterate
+##
+  obj. <- object
   iter <- 0
   while(any(sign(cP[, 2]) != sign(cP.)) && (iter<=maxiter)){
-    iter <- iter+1 
+    iter <- iter+1
 #    obj. <- insert.dierckx(obj.)
     obj. <- insert(obj.)
     cP <- controlPolygon(obj.)
@@ -25,24 +25,27 @@ splineZeros <- function(object, maxiter=10){
   }
 ##
 ## 4.  Use 'uniroot' to obtain the zeros in each interval
-##     containing a sign change in the spline   
+##     containing a sign change in the spline
 ##
   n. <- length(cP.)
   cPs <- sign(cP.)
   signChg <- which(cPs[-1] != cPs[-n.])
   n.sc <- length(signChg)
-  zeros <- array(NA, dim=c(n.sc, 4), dimnames = list(NULL,
-       c("root", "f.root", "iter", "estim.prec") ) )
-#  
+#  zeros <- array(NA, dim=c(n.sc, 4), dimnames = list(NULL,
+#       c("root", "f.root", "iter", "estim.prec") ) )
+#
   predDierckx <- function(x, object)predict.dierckx(object, x)
-#  
-  for(i in 1:n.sc){    
+#
+  for(i in 1:n.sc){
 #    uni. <- uniroot(predict.dierckx, cP[i+0:1], object=object)
     uni. <- uniroot(predDierckx, cP[signChg[i]+0:1], object=object)
-    zeros[i, ] <- unlist(uni.) 
+    if(i<2){
+        zeros <- as.data.frame(uni.)
+    } else zeros <- rbind(zeros, as.data.frame(uni.))
+#    zeros[i, ] <- unlist(uni.)
   }
 ##
 ## 5.  Done
 ##
-  zeros 
+  zeros
 }
